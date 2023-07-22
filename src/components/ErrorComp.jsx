2a8a8ca8577a25c8195c2684023d767ac6dbf0df
Harsh <harshjaiswal40383@gmail.com>
2023-07-23 04:53:55 +0530

    first commit

---
+++ src/components/ErrorComp.jsx
@@ -0,0 +1,21 @@
+import { Alert, AlertIcon } from "@chakra-ui/react";
+import React from "react";
+
+function ErrorComp({ message }) {
+  return (
+    <Alert
+      status="error"
+      position={"fixed"}
+      bottom={"4"}
+      left={"50%"}
+      transform={"translateX(-50%)"}
+      w={"container.lg"}
+
+    >
+      <AlertIcon />
+      {message}
+    </Alert>
+  );
+}
+
+export default ErrorComp;
